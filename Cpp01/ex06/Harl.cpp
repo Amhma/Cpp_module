1ff5fca138ace1cb7fef38f7a5b399f6b8bba2c4
Ammah <amir.mahla@icloud.com>
2022-09-22 21:44:23 +0200

    Cpp01 done

--- Cpp01/ex06/Harl.cpp
+++ Cpp01/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: amahla <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 19:57:50 by amahla            #+#    #+#             */
-/*   Updated: 2022/09/22 20:05:17 by amahla           ###   ########.fr       */
+/*   Updated: 2022/09/22 21:26:07 by amahla           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,31 +29,35 @@ Harl::Harl( void )
 
 void	Harl::_debug( void )
 {
+	cout << "[ DEBUG ]" << endl;
 	cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger." << endl;
-	cout << "I really do !" << endl;
+	cout << "I really do !" << endl << endl;
 }
 
 void	Harl::_info( void )
 {
+	cout << "[ INFO ]" << endl;
 	cout << "I cannot believe adding extra bacon costs more money." << endl;
 	cout << "You didn’t put enough bacon in my burger !" << endl;
-	cout << "If you did, I wouldn’t be asking for more !" << endl;
+	cout << "If you did, I wouldn’t be asking for more !" << endl << endl;
 }
 
 void	Harl::_warning( void )
 {
+	cout << "[ WARNING ]" << endl;
 	cout << "I think I deserve to have some extra bacon for free." << endl;
-	cout << "I’ve been coming for years whereas you started working here since last month." << endl;
+	cout << "I’ve been coming for years whereas you started working here since last month." << endl << endl;
 }
 
 void	Harl::_error( void )
 {
-	cout << "This is unacceptable ! I want to speak to the manager now." << endl;
+	cout << "[ ERROR ]" << endl;
+	cout << "This is unacceptable, I want to speak to the manager now." << endl << endl;
 }
 
 void	Harl::_no_comment( void )
 {
-	std::cerr << "No comment" << endl;
+	std::cerr << "[ Probably complaining about insignificant problems ]" << endl;
 }
 
 void	Harl::complain( std::string level )
@@ -63,5 +67,22 @@ void	Harl::complain( std::string level )
 
 	for ( i = 0; i < 4 && strs[i] != level; i++ )
 		;
-	(this->*_ft_array[i])();
+	switch ( i )
+	{
+		case 0:
+			(this->*_ft_array[i++])();
+			__attribute__ ((fallthrough));
+		case 1:
+			(this->*_ft_array[i++])();
+			__attribute__ ((fallthrough));
+		case 2:
+			(this->*_ft_array[i++])();
+			__attribute__ ((fallthrough));
+		case 3:
+			(this->*_ft_array[i])();
+			break;
+		default:
+			(this->*_ft_array[i])();
+			break;
+	}
 }
--- Cpp01/ex06/main.cpp
+++ Cpp01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amahla <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 20:10:36 by amahla            #+#    #+#             */
-/*   Updated: 2022/09/22 21:38:19 by amahla           ###   ########.fr       */
+/*   Updated: 2022/09/22 21:36:29 by amahla           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,16 @@ int main( int argc, const char *argv[] )
 {
 	Harl	Harl;
 
-	if ( argc == 1 )
-		std::cout << "Statement needed ..." << std::endl;
-	for ( int i(1); i < argc; i++ )
-		Harl.complain( argv[i] );
+	switch ( argc )
+	{
+		case 1:
+			std::cout << "Statement needed ..." << std::endl;
+			break;
+		case 2:
+			Harl.complain( argv[1] );
+			break;
+		default:
+			std::cout << "Too many statements" << std::endl;
+	}
 	return 0;
 }

537b140280232b620cf363ad19788334c96f2515
Ammah <amir.mahla@icloud.com>
2022-09-26 12:01:25 +0200

    Cpp02 fix

--- Cpp02/ex03/bsp.cpp
+++ Cpp02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: amahla <amahla@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 01:45:02 by amahla            #+#    #+#             */
-/*   Updated: 2022/09/26 03:47:08 by amahla           ###   ########.fr       */
+/*   Updated: 2022/09/26 12:00:01 by amahla           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,22 @@
 
 Fixed	vector_routine( Point const u, Point const v, Point const w, Point const point )
 {
-	Point const	vec_1( v - u );
-	Point const	vec_2( point - u );
-	Point const	vec_3( w - u );
+	Point const	vec_1( u - v );
+	Point const	vec_2( u - point );
+	Point const	vec_3( u - w );
 
+	std::cout << "vector vec_2 : " << vec_2.getPointX() << " ";
+	std::cout << "vector vec_2 : " << vec_2.getPointY() << " ";
+	std::cout << "vector vec_2 : " << vec_2.getPointZ() << " " << std::endl;
+	std::cout << "vector vec_1 : " << vec_1.getPointX() << " ";
+	std::cout << "vector vec_1 : " << vec_1.getPointY() << " ";
+	std::cout << "vector vec_1 : " << vec_1.getPointZ() << " " << std::endl;
 	return ( ( vec_1 ^ vec_2 ) * ( vec_2 ^ vec_3 ) );
 }
 
 bool	bsp( Point const a, Point const b, Point const c, Point const point)
 {
-	Fixed const	zero;
+	Fixed const	zero( 0.f );
 
 	if ( vector_routine( a, b, c, point ) > zero
 		&& vector_routine( b, c, a, point ) > zero
--- Cpp02/ex03/main.cpp
+++ Cpp02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amahla <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 18:59:59 by amahla            #+#    #+#             */
-/*   Updated: 2022/09/26 03:42:03 by amahla           ###   ########.fr       */
+/*   Updated: 2022/09/26 11:59:19 by amahla           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ int main( void )
 	Point const a( 1, 1);
 	Point const b( 3, 2 );
 	Point const c( 1, 3 );
-	Point const point( 2, 2 );
+	Point const point( 1.04, 1.04 );
+
+	std::cout << point.getPointX() << std::endl;
 
 	if ( bsp(a, b, c, point ) )
 		std::cout << "The point is in the triangle." << std::endl;

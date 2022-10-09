da7a159cfe657dd19fd85047f5021fc465d91015
Ammah <amir.mahla@icloud.com>
2022-10-09 12:47:44 +0200

    Cpp06 done Array.tpp

--- Cpp07/ex02/headers/Array.hpp
+++ Cpp07/ex02/headers/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: amahla <amahla@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 02:09:42 by amahla            #+#    #+#             */
-/*   Updated: 2022/10/09 03:14:04 by amahla           ###   ########.fr       */
+/*   Updated: 2022/10/09 12:43:15 by amahla           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,66 +22,23 @@ class Array
 
 	public:
 
-		Array( void ) : _array( NULL ), _size( 0 ) {}
+		Array( void );
+		Array( unsigned int n );
+		Array( const Array & rhs );
 
-		Array( unsigned int n ) : _array( new T[n] ), _size( n ) {}
+		~Array( void );
 
-		Array( const Array & rhs ) : _array( NULL ), _size( 0 )
-		{
-			*this = rhs;
-		}
+		Array &	operator=( const Array & rhs );
+		T	& operator[]( unsigned int n );
 
-		~Array( void )
-		{
-			if ( this->_array )
-				delete [] this->_array;
-		}
+		T*				getArray( void ) const;
+		unsigned int	size( void ) const;
 
-		Array &	operator=( const Array & rhs )
-		{
-			if ( this != &rhs )
-			{
-				if ( this->_size != rhs.size() )
-				{
-					this->_size = rhs.size();
-					if ( this->_array )
-						delete [] this->_array;
-					this->_array = new T[this->_size];
-				}
-				setArray( rhs.getArray() );
-			}
-			return ( *this );
-		}
-
-		T	& operator[]( unsigned int n )
-		{
-			if ( n < this->_size )
-				return ( this->_array[n] );
-			throw ( InvalidIndice() );
-			return ( this->_array[0] );
-		}
-
-		T*				getArray( void ) const
-		{
-			return ( this->_array );
-		}
-
-		unsigned int	size( void ) const
-		{
-			return ( this->_size );
-		}
-
-		void			setSize( const unsigned int size ) const
-		{
-			this->_size = size;
-		}
+		void			setSize( const unsigned int size ) const;
 
 		class InvalidIndice : public std::exception
 		{
-			virtual const char	*what( void ) const throw()
-			{
-				return ( "Exception: Invalid index." );
-			}
+			virtual const char*	what( void ) const throw();
 		};
 
 	private:
@@ -89,12 +46,10 @@ class Array
 		T*				_array;
 		unsigned int	_size;
 
-		void			setArray( const T *array ) const
-		{
-			for ( unsigned int i(0); i < this->_size; i++ )
-				this->_array[i] = array[i];
-		}
+		void			setArray( const T *array ) const;
 
 };
+
+# include "Array.tpp"
 
 #endif
---
+++ Cpp07/ex02/headers/Array.tpp
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.tpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amahla <amahla@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/09 02:09:42 by amahla            #+#    #+#             */
+/*   Updated: 2022/10/09 12:46:28 by amahla           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+template< typename T >
+Array<T>::Array( void ) : _array( NULL ), _size( 0 )
+{}
+
+template< typename T >
+Array<T>::Array( unsigned int n ) : _array( new T[n] ), _size( n )
+{}
+
+template< typename T >
+Array<T>::Array( const Array & rhs ) : _array( NULL ), _size( 0 )
+{
+	*this = rhs;
+}
+
+template< typename T >
+Array<T>::~Array( void )
+{
+	if ( this->_array )
+		delete [] this->_array;
+}
+
+template< typename T >
+Array<T> &	Array<T>::operator=( const Array & rhs )
+{
+	if ( this != &rhs )
+	{
+		if ( this->_size != rhs.size() )
+		{
+			this->_size = rhs.size();
+			if ( this->_array )
+				delete [] this->_array;
+			this->_array = new T[this->_size];
+		}
+		setArray( rhs.getArray() );
+	}
+	return ( *this );
+}
+
+template< typename T >
+T	& Array<T>::operator[]( unsigned int n )
+{
+	if ( n < this->_size )
+		return ( this->_array[n] );
+	throw ( InvalidIndice() );
+	return ( this->_array[0] );
+}
+
+template< typename T >
+T*				Array<T>::getArray( void ) const
+{
+	return ( this->_array );
+}
+
+template< typename T >
+unsigned int	Array<T>::size( void ) const
+{
+	return ( this->_size );
+}
+
+template< typename T >
+void			Array<T>::setSize( const unsigned int size ) const
+{
+	this->_size = size;
+}
+
+template< typename T >
+const char*	Array<T>::InvalidIndice::what( void ) const throw()
+{
+	return ( "Exception: Invalid index." );
+}
+
+template< typename T >
+void			Array<T>::setArray( const T *array ) const
+{
+	for ( unsigned int i(0); i < this->_size; i++ )
+		this->_array[i] = array[i];
+}
